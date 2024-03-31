08b04bc6a008f453a82159edde9ace21a275dcc9
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-03-31 15:33:30 +0700

    fix(YT Music - Hide terms container): Patch doesn't require restart to apply the changes
    https://github.com/YT-Advanced/ReX-patches/issues/178

--- app/src/main/java/app/revanced/integrations/music/settings/SettingsEnum.java
+++ app/src/main/java/app/revanced/integrations/music/settings/SettingsEnum.java
@@ -32,7 +32,7 @@ public enum SettingsEnum {
     HIDE_ACCOUNT_MENU_FILTER_STRINGS("revanced_hide_account_menu_filter_strings", STRING, "", 1),
     HIDE_ACCOUNT_MENU_EMPTY_COMPONENT("revanced_hide_account_menu_empty_component", BOOLEAN, FALSE),
     HIDE_HANDLE("revanced_hide_handle", BOOLEAN, TRUE, true),
-    HIDE_TERMS_CONTAINER("revanced_hide_terms_container", BOOLEAN, FALSE, true),
+    HIDE_TERMS_CONTAINER("revanced_hide_terms_container", BOOLEAN, FALSE),
 
 
     // Action Bar
